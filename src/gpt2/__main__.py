629b63cf8fdd6bfef44ecf03bad4cb45dbc9a4e2
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-29 08:41:05 +0900

    Fix argparse params

--- src/gpt2/__main__.py
+++ src/gpt2/__main__.py
@@ -9,8 +9,13 @@ warnings.filterwarnings(action='ignore')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
+        prog='gpt2',
         description='Pytorch implementation of OpenAI GPT-2')
-    subparsers = parser.add_subparsers(required=True)
+    subparsers = parser.add_subparsers(dest='subcommands', required=True)
 
     # Add `train` to the parser.
-    train.add_subparser(subparsers)
+    training.add_subparser(subparsers)
+
+    # Parse arguments and call corresponding function.
+    args = parser.parse_args()
+    args.func(args)
