64f987fb5150eb172ae4a856711cf58c667f8b35
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-29 23:02:38 +0900

    Newly implement tokenizer without library

--- src/gpt2/data/tokenization.py
+++ src/gpt2/data/tokenization.py
@@ -1,40 +1,93 @@
-from tokenizers import (models,
-                        decoders,
-                        normalizers,
-                        pre_tokenizers,
-                        implementations,
-                        Tokenizer as CoreTokenizer)
+from .vocabulary import Vocabulary
+import regex as re
 from typing import List
 
+_CHINESE_CHAR_RANGE = ('\u4e00-\u9fff\u3400-\u4dbf\U00020000-\U0002a6df'
+                       '\U0002a700-\U0002b73f\U0002b740-\U0002b81f'
+                       '\U0002b820-\U0002ceaf\uf900-\ufaff'
+                       '\U0002f800-\U0002fa1f')
+_PUNCTUATION_RANGE = '\\p{P}\x21-\x2f\x3a-\x40\x5b-\x60\x7b-\x7e'
 
-class Tokenizer(implementations.BaseTokenizer):
-    """Implementation of subword tokenizer.
+
+class Tokenizer(object):
+    """Wordpiece-based subword tokenizer.
 
     Arguments:
-        vocab (str): Vocabulary file path.
-        unk_token (str): Unknown token name.
-        special_tokens (list): The list of speical token names except unknown
-            token.
-
-    Note:
-        * Unknown token is a special token as well, but it must be specified
-          separately from other speical tokens.
-        * In case of GPT-2, **WordPiece** model is used in tokenization.
+        vocab (Vocabulary): The vocabulary object.
+        special_tokens (list): The list of special tokens.
+        max_word_len (int): The maximum length of each subword.
     """
     def __init__(self,
-                 vocab: str,
-                 unk_token: str,
-                 special_tokens: List[str] = []):
-        # Use WordPiece tokenizer.
-        tokenizer = CoreTokenizer(models.WordPiece(vocab, unk_token=unk_token))
-        tokenizer.add_special_tokens([unk_token] + special_tokens)
+                 vocab: Vocabulary,
+                 special_tokens: List[str] = [],
+                 max_word_len: int = 100):
+        self.vocab = vocab
+        self.special_tokens = special_tokens
+        self.max_word_len = max_word_len
 
-        # Use BERT-specific normalizer and pre-tokenizer.
-        tokenizer.normalizer = normalizers.BertNormalizer(strip_accents=False)
-        tokenizer.pre_tokenizer = pre_tokenizers.BertPreTokenizer()
+    def encode(self, text: str, unk_token: str = '<unk>') -> List[str]:
+        """Encode sentence to subword tokens.
 
-        # Use WordPiece decoder.
-        tokenizer.decoder = decoders.WordPiece(prefix='##')
+        Arguments:
+            text (str): Input sentence.
+            unk_token (str): Unknown token name.
 
-        # Initialize tokenizer.
-        super().__init__(tokenizer)
+        Returns:
+            A list of subword tokens."""
+        return [token
+                for normalized in self._normalize(text)
+                for token in self._tokenize(normalized, unk_token)]
+
+    def _normalize(self, text: str) -> List[str]:
+        # Clear text by normalizing whitespace characters and removing control
+        # characters.
+        text = ' '.join(re.sub('[\x00\uFFFD\\p{C}]', '', t)
+                        for t in text.split())
+
+        # Insert whitespaces between chinese characters.
+        text = re.sub(f'([{_CHINESE_CHAR_RANGE}])', r' \1 ', text)
+
+        normalized = []
+        for t in text.split():
+            if t in self.special_tokens:
+                # Do not split special tokens.
+                normalized.append(t)
+            else:
+                # Prevent treating tokens with punctuations.
+                normalized += re.split(f'([{_PUNCTUATION_RANGE}])', t.lower())
+
+        return ' '.join(normalized).split()
+
+    def _tokenize(self, text: str, unk_token: str) -> List[str]:
+        subwords = []
+
+        for token in text.split():
+            # Skip too long tokens.
+            if len(token) > 100:
+                subwords.append(unk_token)
+                continue
+
+            children = []
+            while token and token != '##':
+                current, token = token, ''
+
+                while current and current != '##':
+                    # If subword is in vocabulary, add to list and re-calibrate
+                    # the target token.
+                    if current in self.vocab:
+                        children.append(current)
+                        token = '##' + token
+                        break
+
+                    # If subword is not in vocabulary, reduce the search range
+                    # of subword and test it again.
+                    current, token = current[:-1], current[-1] + token
+
+                # Process current token as `unknown` since there is no any
+                # proper tokenization (greedy).
+                if not current:
+                    children, token = None, None
+
+            subwords += children or [unk_token]
+
+        return subwords
