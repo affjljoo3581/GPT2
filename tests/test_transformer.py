c6dc78a37fe3e390de1faf807b0120ba681a1388
affjljoo3581 <affjljoo3581@gmail.com>
2020-06-24 16:26:31 +0900

    Add new test codes for transformer model

---
+++ tests/test_transformer.py
@@ -0,0 +1,89 @@
+import torch
+from gpt2.modeling.transformer import TransformerLayer, Transformer
+
+
+def test_the_shape_from_transformer_layer():
+    # Create new transformer layer.
+    layer = TransformerLayer(heads=2, dims=16, rate=4).eval()
+
+    # Check shape-invariance for various shape.
+    input_tensor = torch.zeros((10, 16))
+    output_tensor, past = layer(input_tensor)
+    assert output_tensor.shape == input_tensor.shape
+    assert past[0].shape == input_tensor.shape
+    assert past[1].shape == input_tensor.shape
+
+    input_tensor = torch.zeros((3, 2, 10, 16))
+    output_tensor, past = layer(input_tensor)
+    assert output_tensor.shape == input_tensor.shape
+    assert past[0].shape == input_tensor.shape
+    assert past[1].shape == input_tensor.shape
+
+    # Test for using `past` tensors.
+    input_tensor = torch.zeros((3, 2, 5, 16))
+    output_tensor, past = layer(input_tensor, past)
+    assert output_tensor.shape == input_tensor.shape
+    assert past[0].shape == (3, 2, 15, 16)
+    assert past[1].shape == (3, 2, 15, 16)
+
+    # Test for masking tensor.
+    input_tensor = torch.zeros((3, 2, 3, 16))
+    masking_tensor = torch.zeros((3, 2, 3, 18), dtype=torch.bool)
+    output_tensor, past = layer(input_tensor, past, masking_tensor)
+    assert output_tensor.shape == input_tensor.shape
+    assert past[0].shape == (3, 2, 18, 16)
+    assert past[1].shape == (3, 2, 18, 16)
+
+
+def test_the_shape_from_transformer_model():
+    # Create transformer model.
+    model = Transformer(layers=2,
+                        pad_idx=0,
+                        words=80,
+                        seq_len=100,
+                        heads=2,
+                        dims=16,
+                        rate=4,
+                        bidirectional=False)
+
+    # Check if the model predicts well for single vector.
+    input_tensor = torch.randint(80, (10,), dtype=torch.long)
+    output_tensor, past = model(input_tensor)
+    assert output_tensor.shape == (10, 80)
+    assert len(past) == 2
+    for p in past:
+        assert p[0].shape == (10, 16)
+        assert p[1].shape == (10, 16)
+
+    # Check if the model predicts well for multi-dimensional tensor.
+    input_tensor = torch.randint(80, (2, 7, 5, 10), dtype=torch.long)
+    output_tensor, past = model(input_tensor)
+    assert output_tensor.shape == (2, 7, 5, 10, 80)
+    assert len(past) == 2
+    for p in past:
+        assert p[0].shape == (2, 7, 5, 10, 16)
+        assert p[1].shape == (2, 7, 5, 10, 16)
+
+    # Test for single vector with `past` tensors.
+    input_tensor = torch.randint(80, (2, 7, 5, 10), dtype=torch.long)
+    _, past = model(input_tensor)
+    input_tensor = torch.randint(80, (2, 7, 5, 7), dtype=torch.long)
+    output_tensor, past = model(input_tensor, past)
+
+    assert output_tensor.shape == (2, 7, 5, 7, 80)
+    assert len(past) == 2
+    for p in past:
+        assert p[0].shape == (2, 7, 5, 17, 16)
+        assert p[1].shape == (2, 7, 5, 17, 16)
+
+    # Consider the case of generating sentence.
+    past = None
+    for _ in range(10):
+        input_tensor = torch.randint(80, (1,), dtype=torch.long)
+        output_tensor, past = model(input_tensor, past)
+
+    assert output_tensor.shape == (1, 80)
+    assert len(past) == 2
+    for p in past:
+        assert p[0].shape == (10, 16)
+        assert p[1].shape == (10, 16)
