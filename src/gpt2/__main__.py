6075beff37c354fec57b4a068b96f0e5c48ef155
affjljoo3581 <affjljoo3581@gmail.com>
2020-06-24 16:20:01 +0900

    Add main script

--- src/gpt2/__main__.py
+++ src/gpt2/__main__.py
@@ -1,6 +1,5 @@
 import argparse
-from . import training, generation, visualization
-
+from . import train
 
 # Ignore warnings.
 import warnings
@@ -10,18 +9,12 @@ warnings.filterwarnings(action='ignore')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='gpt2',
-        description='Pytorch implementation of OpenAI GPT-2')
+        description='PyTorch implementation of OpenAI GPT-2')
     subparsers = parser.add_subparsers(dest='subcommands', required=True)
 
-    # Add `train` to the parser.
-    training.add_subparser(subparsers)
+    # Add `train` keyword to the parser.
+    train.add_subparser(subparsers)
 
-    # Add `generate` to the parser.
-    generation.add_subparser(subparsers)
-
-    # Add `visualize` to the parser.
-    visualization.add_subparser(subparsers)
-
-    # Parse arguments and call corresponding function.
+    # Parse passed arguments and call corresponding function.
     args = parser.parse_args()
     args.func(args)
