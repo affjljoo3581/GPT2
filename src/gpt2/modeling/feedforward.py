7dc1ddfb36d825d50517d296fdc25e83b2f78cb8
affjljoo3581 <affjljoo3581@gmail.com>
2020-06-25 07:46:18 +0900

    Change to use Swish activation

--- src/gpt2/modeling/feedforward.py
+++ src/gpt2/modeling/feedforward.py
@@ -1,4 +1,14 @@
+import torch
 import torch.nn as nn
+
+
+class Swish(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * self.sigmoid(x)
 
 
 class PositionwiseFeedForward(nn.Sequential):
@@ -13,6 +23,6 @@ class PositionwiseFeedForward(nn.Sequential):
     def __init__(self, dims: int, rate: int = 4, dropout: float = 0.1):
         super().__init__(
             nn.Linear(dims, dims * rate),
-            nn.GELU(),
+            Swish(),
             nn.Dropout(dropout),
             nn.Linear(dims * rate, dims))
