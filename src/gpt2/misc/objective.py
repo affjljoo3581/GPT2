04f4f6a3be6b25c5373120bb8c3d06c5ed56014e
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 22:22:11 +0900

    Add empty preserving methods

--- src/gpt2/misc/objective.py
+++ src/gpt2/misc/objective.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from typing import Dict, Any
 
 
 class Objective(object):
@@ -9,6 +10,12 @@ class Objective(object):
     def loss(self, inputs: torch.Tensor, outputs: torch.Tensor
              ) -> torch.Tensor:
         raise NotImplementedError()
+
+    def state_dict(self) -> Dict[str, Any]:
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        pass
 
 
 class LMObjective(Objective):
