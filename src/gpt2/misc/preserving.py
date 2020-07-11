2507307655faa005b46a63ff3f05656abcac2c39
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 22:02:02 +0900

    Add temporarily test code

--- src/gpt2/misc/preserving.py
+++ src/gpt2/misc/preserving.py
@@ -7,6 +7,7 @@ class Preservable(object):
         def _get_state(obj: object):
             # If object has `state_dict` method, use it rather than dump the
             # value directly.
+            print(obj, getattr(obj, 'state_dict', None) is not None)
             return (obj.state_dict()
                     if getattr(obj, 'state_dict', None)
                     else obj)
