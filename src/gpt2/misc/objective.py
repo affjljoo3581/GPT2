9b6f2d9c90eceaf91fe87cc18a1d98744307753b
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 19:21:56 +0900

    Change the method name to `loss`

--- src/gpt2/misc/objective.py
+++ src/gpt2/misc/objective.py
@@ -6,8 +6,8 @@ class Objective(object):
     def __init__(self, model: nn.Module):
         self.model = model
 
-    def __call__(self, inputs: torch.Tensor, outputs: torch.Tensor
-                 ) -> torch.Tensor:
+    def loss(self, inputs: torch.Tensor, outputs: torch.Tensor
+             ) -> torch.Tensor:
         raise NotImplementedError()
 
 
@@ -17,7 +17,7 @@ class LMObjective(Objective):
         self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx,
                                              reduction='mean')
 
-    def __call__(self, inputs: torch.Tensor, outputs: torch.Tensor
-                 ) -> torch.Tensor:
+    def loss(self, inputs: torch.Tensor, outputs: torch.Tensor
+             ) -> torch.Tensor:
         logits, _ = self.model(inputs, None)
         return self.criterion(logits.transpose(1, 2), outputs)
