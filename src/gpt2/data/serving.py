e7245be359cfd2d6f3633c523d5f3b2c2b591c12
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 17:59:57 +0900

    Change to use interface class

--- src/gpt2/data/serving.py
+++ src/gpt2/data/serving.py
@@ -1,48 +1,56 @@
 import torch
-from .vocabulary import Vocabulary
-from typing import Optional, Union, List, Dict, Any
+from .vocabulary import Vocab
+from typing import Optional, Dict, List, Any
 
 
-class DataLoader(object):
-    def __init__(self, vocab: Vocabulary, corpus: str, seq_len: int):
+class Dataset(object):
+    def skip(self, count: int):
+        raise NotImplementedError()
+
+    def fetch(self, batch: Optional[int] = None, device: Optional[str] = None
+              ) -> Dict[str, torch.Tensor]:
+        raise NotImplementedError()
+
+
+class TokenizedCorpusDataset(Dataset):
+    def __init__(self, vocab: Vocab, corpus_path: str, seq_len: int):
         self.vocab = vocab
-        self.corpus_fp = open(corpus, 'r', encoding='utf-8')
+        self.corpus_fp = open(corpus_path, 'r', encoding='utf-8')
         self.seq_len = seq_len
 
-    def close(self):
-        self.corpus_fp.close()
+    def skip(self, count: int):
+        if not self.corpus_fp.readline():
+            self.corpus_fp.seek(0)
+            self.corpus_fp.readline()
 
-    def _fetch_one(self) -> Dict[str, torch.Tensor]:
+    def _fetch_one(self) -> Dict[str, List[int]]:
         while True:
             line = self.corpus_fp.readline()
             if not line:
                 self.corpus_fp.seek(0)
                 continue
 
-            # Map each subword to its index.
+            # Map each subword to its token index.
             indices = [self.vocab[t] for t in line.split()]
             if len(indices) > self.seq_len - 2:
                 continue
 
-            # Add speical tokens to the sequence.
+            # Add special tokens to the sequence.
             indices = [self.vocab.bos_idx] + indices + [self.vocab.eos_idx]
             indices += ([self.vocab.pad_idx]
                         * (self.seq_len - len(indices) + 1))
 
             return {'input': indices[:-1], 'output': indices[1:]}
 
-    def fetch(self,
-              batch: Optional[int] = None,
-              device: Optional[Union[str, torch.device]] = None
-              ) -> Union[Dict[str, torch.Tensor],
-                         List[Dict[str, torch.Tensor]]]:
+    def fetch(self, batch: Optional[int] = None, device: Optional[str] = None
+              ) -> Dict[str, torch.Tensor]:
         if batch is None:
             data = self._fetch_one()
         else:
             data = [self._fetch_one() for _ in range(batch)]
             data = {k: [d[k] for d in data] for k in data[0]}
 
-        # Cast each sequence to tensor.
+        # Cast sequences to tensors.
         return {k: torch.tensor(v, dtype=torch.long, device=device)
                 for k, v in data.items()}
 
