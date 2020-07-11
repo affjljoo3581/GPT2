590fe31dbdb953a2be249f366bf374eaf950fc00
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 18:00:49 +0900

    Change to use interface class

---
+++ src/gpt2/misc/objective.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class Objective(object):
+    def __init__(self, model: nn.Module):
+        self.model = model
+
+    def __call__(self, inputs: torch.Tensor, outputs: torch.Tensor
+                 ) -> torch.Tensor:
+        raise NotImplementedError()
+
+
+class LMObjective(Objective):
+    def __init__(self, model: nn.Module, pad_idx: int = 0):
+        super().__init__(model)
+        self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx,
+                                             reduction='mean')
+
+    def __call__(self, inputs: torch.Tensor, outputs: torch.Tensor
+                 ) -> torch.Tensor:
+        logits, _ = self.model(inputs, None)
+        return self.criterion(logits.transpose(1, 2), outputs)
