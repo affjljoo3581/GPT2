f20ab495963f84b9c9e878fb1830811d0885b4fe
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-11 22:28:12 +0900

    Add test codes

--- src/gpt2/misc/preserving.py
+++ src/gpt2/misc/preserving.py
@@ -19,12 +19,15 @@ class Preservable(object):
     def restore(self, checkpoint: str, map_location: Optional[str] = None):
         ckpt = torch.load(checkpoint, map_location=map_location)
         for k, v in ckpt.items():
+            print(f'==========={k}=========')
             if getattr(getattr(self, k), 'load_state_dict', None):
                 # If object has `load_state_dict` method, use it rather than
                 # assign the value directly.
                 getattr(self, k).load_state_dict(v)
+                print('load with load_state_dict')
             else:
                 setattr(self, k, v)
+                print(f'just copy that. {v}')
 
         # Remove used checkpoint object and clear cuda cache to prevent out of
         # memory error.
