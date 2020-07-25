244340a42c99eb55edb4bea4cdbacdcd94163684
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-25 23:58:41 +0900

    Change some codes

--- src/gpt2/data/vocabulary.py
+++ src/gpt2/data/vocabulary.py
@@ -13,40 +13,39 @@ class Vocab(object):
         self.eos_token = eos_token
         self.pad_token = pad_token
 
-        self.additional_tokens = [bos_token, eos_token, pad_token]
-
         with open(vocab_path, 'r', encoding='utf-8') as fp:
-            self.words = self.additional_tokens + fp.read().split()
+            additional_tokens = [bos_token, eos_token, pad_token]
+
+            # The additional tokens would be inserted before the words.
+            self.words = additional_tokens + fp.read().split()
             self.vocab = {word: i for i, word in enumerate(self.words)}
 
-    def __getitem__(self, token: Union[int, str]) -> Union[str, int]:
-        if isinstance(token, str):
-            return self.vocab[token]
+    def __getitem__(self, idx_or_token: Union[int, str]) -> Union[str, int]:
+        if isinstance(idx_or_token, str):
+            return self.vocab[idx_or_token]
         else:
-            return self.words[token]
+            return self.words[idx_or_token]
 
     def __contains__(self, token: str) -> bool:
         return token in self.words
 
     def __len__(self) -> int:
+        # Note that vocabulary size must be a multiple of 8 although the actual
+        # number of words is less than it.
         return (len(self.words) + 7) // 8 * 8
 
     @property
-    def specials(self) -> int:
-        return len(self.additional_tokens)
-
-    @property
     def unk_idx(self) -> int:
-        return self[self.unk_token]
+        return self.vocab[self.unk_token]
 
     @property
     def bos_idx(self) -> int:
-        return self[self.bos_token]
+        return self.vocab[self.bos_token]
 
     @property
     def eos_idx(self) -> int:
-        return self[self.eos_token]
+        return self.vocab[self.eos_token]
 
     @property
     def pad_idx(self) -> int:
-        return self[self.pad_token]
+        return self.vocab[self.pad_token]
