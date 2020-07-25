1509bf1c312806aa0858525e53e9669771faa284
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-25 23:54:41 +0900

    Rename the implementation script file name

---
+++ src/gpt2/data/corpus.py
@@ -0,0 +1,51 @@
+import torch
+from gpt2.data import Dataset, Vocab
+from typing import Dict, Any, List, Optional
+
+
+class TokenizedCorpus(Dataset):
+    def __init__(self, corpus_path: str, vocab: Vocab, seq_len: int):
+        self.corpus_fp = open(corpus_path, 'r', encoding='utf-8')
+        self.vocab = vocab
+        self.seq_len = seq_len
+
+    def skip(self, count: int):
+        for _ in range(count):
+            if not self.corpus_fp.readline():
+                # If all sequences are fetched, move to the first of corpus.
+                self.corpus_fp.seek(0)
+                self.corpus_fp.readline()
+
+    def _fetch_one(self) -> Dict[str, List[int]]:
+        while True:
+            # Read subword-tokenized sequence from corpus.
+            line = self.corpus_fp.readline()
+            if not line:
+                self.corpus_fp.seek(0)
+                continue
+
+            # Use token indices rather than the token names directly.
+            indices = [self.vocab[t] for t in line.split()]
+            if len(indices) + 2 > self.seq_len:
+                continue
+
+            # Decorate the sequence with additional tokens.
+            indices = [self.vocab.bos_idx] + indices + [self.vocab.eos_idx]
+            indices += [self.vocab.pad_idx] * (self.seq_len - len(indices) + 1)
+
+            return {'input': indices[:-1], 'output': indices[1:]}
+
+    def fetch(self, batch: Optional[int] = None) -> Dict[str, torch.Tensor]:
+        if batch is None:
+            data = self._fetch_one()
+        else:
+            data = [self._fetch_one() for _ in range(batch)]
+            data = {k: [d[k] for d in data] for k in data[0]}
+
+        return {k: torch.tensor(v, dtype=torch.long) for k, v in data.items()}
+
+    def where(self) -> Dict[str, Any]:
+        return {'offset': self.corpus_fp.tell()}
+
+    def assign(self, where: Dict[str, Any]):
+        self.corpus_fp.seek(where['offset'])
