f0775d0bbb9bf2444fed17cd5e1857bcb29c0616
affjljoo3581 <affjljoo3581@gmail.com>
2020-08-01 19:11:46 +0900

    Add evaluation module in argument parser

--- src/gpt2/__main__.py
+++ src/gpt2/__main__.py
@@ -1,5 +1,8 @@
 import argparse
-from gpt2 import train_model, generate_sentences, visualize_metrics
+from gpt2 import (train_model,
+                  evaluate_model,
+                  generate_sentences,
+                  visualize_metrics)
 
 
 if __name__ == '__main__':
@@ -9,6 +12,7 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(dest='subcommands', required=True)
 
     train_model.add_subparser(subparsers)
+    evaluate_model.add_subparser(subparsers)
     generate_sentences.add_subparser(subparsers)
     visualize_metrics.add_subparser(subparsers)
 
