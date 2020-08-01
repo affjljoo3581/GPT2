78806a3e032c8a0346b8dbdf0178db2ee29d587b
affjljoo3581 <affjljoo3581@gmail.com>
2020-08-01 19:25:03 +0900

    Add `total_steps` and implement iterator

--- src/gpt2/evaluation/configuration.py
+++ src/gpt2/evaluation/configuration.py
@@ -1,7 +1,18 @@
+from typing import Iterator
+
 
 class EvaluateConfig(object):
     def __init__(self,
                  batch_eval: int,
+                 total_steps: int,
                  use_gpu: bool):
         self.batch_eval = batch_eval
+        self.total_steps = total_steps
         self.use_gpu = use_gpu
+
+    def iterate(self) -> Iterator:
+        if self.total_steps == -1:
+            while True:
+                yield
+        else:
+            yield from range(self.total_steps)
