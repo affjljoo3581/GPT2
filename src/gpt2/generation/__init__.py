c1ac444af7fa3191bfcf5376d09d5cf34a564162
affjljoo3581 <affjljoo3581@gmail.com>
2020-08-01 17:05:52 +0900

    Update modules

--- src/gpt2/generation/__init__.py
+++ src/gpt2/generation/__init__.py
@@ -1,2 +1,3 @@
-from gpt2.generation.tokenization import Tokenizer
+from gpt2.generation.specification import GenerationSpec
+from gpt2.generation.configuration import GenerateConfig
 from gpt2.generation.generation import Generator
