376e45a66bda95100af7c12b4bb3e59423881b18
affjljoo3581 <affjljoo3581@gmail.com>
2020-08-01 17:06:02 +0900

    Add new test codes for generator

--- tests/generation/test_generation.py
+++ tests/generation/test_generation.py
@@ -1,12 +1,13 @@
 import torch
 import numpy as np
 from gpt2.modeling import Transformer
-from gpt2.generation import Generator
+from gpt2.generation import GenerationSpec, GenerateConfig, Generator
 
 
 def test_generator_sample_from_top_p():
-    generator = Generator(vocab=None, tokenizer=None, model=None, seq_len=None,
-                          top_p=0.5, use_gpu=None)
+    generator = Generator(
+        spec=None,
+        config=GenerateConfig(seq_len=0, nucleus_prob=0.5, use_gpu=False))
 
     probs = torch.tensor([0.1, 0.2, 0.15, 0.08, 0.25, 0.22])
     for _ in range(100):
@@ -14,10 +15,16 @@ def test_generator_sample_from_top_p():
 
 
 def test_generator_predict_probs_output_shape():
-    model = Transformer(layers=2, pad_idx=0, words=80, seq_len=16, heads=2,
-                        dims=16, rate=4, dropout=0, bidirectional=False).eval()
-    generator = Generator(vocab=None, tokenizer=None, model=model,
-                          seq_len=None, top_p=0.5, use_gpu=None)
+    spec = GenerationSpec()
+    spec.construct_model = lambda: Transformer(
+        layers=2, pad_idx=0, words=80, seq_len=16, heads=2, dims=16, rate=4,
+        dropout=0, bidirectional=False)
+
+    # Create generator with simple specification.
+    generator = Generator(
+        spec=spec,
+        config=GenerateConfig(seq_len=0, nucleus_prob=0.5, use_gpu=False))
+    generator.initialize()
 
     probs1, past1 = generator._predict_probs(
         [np.random.randint(80) for _ in range(8)])
@@ -33,3 +40,21 @@ def test_generator_predict_probs_output_shape():
     for p in past2:
         assert p[0].shape == (9, 16)
         assert p[1].shape == (9, 16)
+
+
+def test_generator_generate():
+    spec = GenerationSpec()
+    spec.construct_model = lambda: Transformer(
+        layers=2, pad_idx=0, words=80, seq_len=16, heads=2, dims=16, rate=4,
+        dropout=0, bidirectional=False)
+    spec.encode_context = lambda context: list(range(len(context.split())))
+    spec.decode_tokens = lambda tokens: ' '.join(str(t) for t in tokens)
+
+    # Create generator with simple specification.
+    generator = Generator(
+        spec=spec,
+        config=GenerateConfig(seq_len=50, nucleus_prob=0.5, use_gpu=False))
+    generator.initialize()
+
+    for t in generator.generate('a b c d e').split():
+        assert int(t) < 80
