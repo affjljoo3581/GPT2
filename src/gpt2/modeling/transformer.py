Fix gradient checkpointing bugs

src/gpt2/modeling/transformer.py
@@ -5,7 +5,7 @@ from gpt2.utils.fusing import LayerNorm
 from gpt2.modeling import (PadMasking, FutureMasking, AttentionLayer, Past,
                            PositionalEmbedding, TokenEmbedding,
                            PositionwiseFeedForward)
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Union
 
 
 class TransformerLayer(nn.Module):
@@ -34,8 +34,9 @@ class TransformerLayer(nn.Module):
     def forward(self,
                 x: torch.Tensor,
                 past: Optional[Past] = None,
-                mask: Optional[torch.Tensor] = None
-                ) -> Tuple[torch.Tensor, Past]:
+                mask: Optional[torch.Tensor] = None,
+                return_past: bool = True
+                ) -> Union[torch.Tensor, Tuple[torch.Tensor, Past]]:
         # Layer normalizations are performed before the layers respectively.
         a = self.ln_attn(x)
         a, past = self.attn(a, a, a, past, mask)
@@ -43,7 +44,9 @@ class TransformerLayer(nn.Module):
         x = x + a
         x = x + self.ff(self.ln_ff(x))
 
-        return x, past
+        if return_past:
+            return x, past
+        return x
 
 
 class Transformer(nn.Module):
@@ -101,11 +104,8 @@ class Transformer(nn.Module):
         present = []
         for i, transformer in enumerate(self.transformers):
             if use_grad_ckpt:
-                def custom_forward(x, past, mask):
-                    return transformer(x, past, mask)[0]
-
                 x = torch.utils.checkpoint.checkpoint(
-                    custom_forward,
+                    lambda *inputs: transformer(*inputs, return_past=False),
                     x, past[i] if past is not None else None, mask)
             else:
                 x, p = transformer(